0bfd86959ce06cbff16614433bd73947b266e8e9
Phongzau <phongzau2004@gmail.com>
2024-09-15 15:27:29 +0700

    Admin, User, Vendor Dashboard Analytics

--- app/Http/Controllers/Backend/Vendor/VendorProfileController.php
+++ app/Http/Controllers/Backend/Vendor/VendorProfileController.php
@@ -4,11 +4,62 @@ namespace App\Http\Controllers\Backend\Vendor;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use File;
 
 class VendorProfileController extends Controller
 {
     public function index()
     {
         return view('vendor.dashboard.profile');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:100'],
+            'email' => ['required', 'email', 'unique:users,email,' . Auth::user()->id],
+            'image' => ['image', 'max:2048'],
+        ]);
+
+        // Lấy ra người dùng hiện tại
+        $user = Auth::user();
+
+        // Kiểm tra có file ảnh đẩy lên không
+        if ($request->hasFile('image')) {
+            // Kiểm tra lại file public/uploads có ảnh cũ không nếu có thì xóa
+            if (File::exists(public_path($user->image))) {
+                File::delete(public_path($user->image));
+            }
+
+            $image = $request->image;
+            $imageName = rand() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('uploads'), $imageName);
+
+            $path = "/uploads/" . $imageName;
+            $user->image = $path;
+        }
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        toastr()->success('Profile updated successfully');
+        return redirect()->back();
+        // dd($request->all());
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $request->validate([
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', 'confirmed', 'min:8']
+        ]);
+
+        $request->user()->update([
+            'password' => bcrypt($request->password),
+        ]);
+
+        toastr()->success('Profile Password Updated Successfully!');
+        return redirect()->back();
     }
 }
